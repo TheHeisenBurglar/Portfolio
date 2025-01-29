c4fe0301a5e54b3a21a88ce3c3b88e3255d462bb
TheHeisenBurglar <vidgames16@gmail.com>
2025-01-29 16:21:12 -0500

    Made singlepage wihtout api dependancy

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,13 +1,1 @@
-import { GET } from "./api/a2/+server";
-
-export async function load() {
-    const date = new Date()
-    const year = date.getFullYear()
-    const res = await GET()
-    const artifacts = await res.json();
-    return {
-        year, artifacts
-    }
-}
-
 export const prerender = true;
